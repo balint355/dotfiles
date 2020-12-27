9142084c4c463fd8f45cca92b43806d0862e9c38
balint355 <mbalint314@gmail.com>
2020-12-27 15:53:48 +0100

    update config script and config structure

--- setup-config.fish
+++ setup-config.fish
@@ -7,13 +7,25 @@ else
     set config_dir ~/.config
 end
 
-for i in config/*
-    set name (basename $i) 
-    set dir $config_dir/$name
+function copy_config
+    for i in config/$argv[1]/*
+        set name (basename $i)
+        set dir $config_dir/$name
 
-    if test -e $dir
-        rm -r $dir
+        if test ! -e $dir
+            mkdir $dir
+        end
+
+        cp $i/* $dir
     end
+end
 
-    ln -rs $i $config_dir
+copy_config base
+
+if test (count $argv) -ge 1
+    if test -e config/$argv[1]
+        copy_config $argv[1]
+    else
+        echo "WARNING: config folder $argv[1] doesn't exist"
+    end
 end
