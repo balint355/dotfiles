1c6ece9a870955241fa2192ce454eb6104611fe3
Máhonfai Bálint <mbalint314@gmail.com>
2021-07-06 16:10:25 +0200

    update config script

--- setup-config.fish
+++ setup-config.fish
@@ -11,20 +11,30 @@ end
 
 function copy_config
     if test ! -e $argv[2]
-        sudo mkdir $argv[2]
+        mkdir $argv[2]
     end
     
+    for i in $argv[1]/$argv[3]/*
+        cp -r $i $argv[2]
+    end
+end
+
+function copy_config_sudo
+    if test ! -e $argv[2]
+        sudo mkdir $argv[2]
+    end
+
     for i in $argv[1]/$argv[3]/*
         sudo cp -r $i $argv[2]
     end
 end
 
-copy_config etc $etc_dir base
+copy_config_sudo etc $etc_dir base
 copy_config config $config_dir base
 
 if test (count $argv) -ge 1
     if test -d etc/$argv[1]
-        copy_config etc $etc_dir $argv[1]
+        copy_config_sudo etc $etc_dir $argv[1]
     end
     
     if test -d config/$argv[1]
